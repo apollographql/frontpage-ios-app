bff57dd437f045112b107b8f66b7d0da830067e6
Martijn Walraven <martijn@martijnwalraven.com>
2016-10-14 13:31:31 -0700

    Add visible touches for demo purposes

--- FrontPage/AppDelegate.swift
+++ FrontPage/AppDelegate.swift
@@ -6,9 +6,10 @@ let apollo = ApolloClient(url: URL(string: "http://192.168.2.22:8080/graphql")!)
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-  var window: UIWindow?
+  var window: UIWindow? = MBFingerTipWindow(frame: UIScreen.main.bounds)
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    (window as! MBFingerTipWindow).alwaysShowTouches = true
     return true
   }
 }
